3b0c7f51472f94903e3df6131645e25ed2b07b4a
Eunice Jacob <jacobeunice40@gmail.com>
2025-04-01 13:51:00 +0100

    initial setup

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const configs = {
+  SERVER_PORT: process.env.SERVER_PORT || 3000,
+
+  JWT_SECRET: process.env.JWT_SECRET || 'test',
+
+  MONGODB_URL: process.env.MONGODB_URL || '',
+
+  REDIS_URL: process.env.REDIS_URL || '',
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { configs } from './config';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.set('trust proxy', 1);
+
+  app.setGlobalPrefix('/v1');
+
+  app.enableCors({
+    credentials: true,
+    origin: function (origin, callback) {
+      callback(null, true);
+    },
+  });
+
+  app.use(cookieParser());
+
+  const port = configs.SERVER_PORT || 3000;
+
+  await app.listen(port, async () => {
+    console.log(
+      `The server is running on ${port} port: http://localhost:${port} `,
+    );
+  });
 }
 bootstrap();
